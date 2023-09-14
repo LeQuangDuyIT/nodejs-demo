d883ab5e512bc9b378ec05c3a4a10a800b3063e2
lqduycp <lqduycp@gmail.com>
2023-09-14 11:35:18 +0700

    Static file & SCSS

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const PORT = 3001;
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.engine('hbs', engine({ extname: '.hbs' }));
 app.set('view engine', 'hbs');
 app.set('views', path.join(__dirname, 'resources/views'));
