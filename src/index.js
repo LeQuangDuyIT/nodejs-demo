805dd718146c63dfe1786d40e38df99c78622733
lqduycp <lqduycp@gmail.com>
2023-09-14 10:41:25 +0700

    Initial nodejs project

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import express from 'express';
+import { engine } from 'express-handlebars';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const PORT = 3001;
+
+app.engine('hbs', engine({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', (_, res) => {
+  res.render('home');
+});
+
+app.listen(PORT, () => {
+  console.log('Welcome to port:', PORT);
+});
